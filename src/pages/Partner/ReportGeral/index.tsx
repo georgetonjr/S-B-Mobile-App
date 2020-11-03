571e3655d10027d6b385ad8754a9fd2626094662
georgetonjr <37754623+georgetonjr@users.noreply.github.com>
2020-11-03 16:04:37 -0300

    add new screen
    Co-Authored-By: carlos511 <38634751+carlos511@users.noreply.github.com>

---
+++ src/pages/Partner/ReportGeral/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+// import { Container } from './styles';
+
+const ReportGeral: React.FC = () => {
+  return (
+    <View>
+      <Text>
+        tela do parceiro
+      </Text>
+    </View>
+  );
+}
+
+export default ReportGeral;
--- src/routes/Partner.routes.tsx
+++ src/routes/Partner.routes.tsx
@@ -7,6 +7,7 @@ import Products from '../pages/Partner/Products';
 import RProduct from '../pages/Partner/RProduct';
 import RSale from '../pages/Partner/RSale';
 import ValidQRCode from '../pages/Partner/ValidQrCode';
+import report from '../pages/Partner/ReportGeral';
 
 const PartnerStack = createBottomTabNavigator();
 
@@ -16,7 +17,8 @@ const PartnerRoutes: React.FC = () => (
     <PartnerStack.Screen name="Produtos" component={Products} />
     <PartnerStack.Screen name="Cadastrar Produto" component={RProduct} />
     <PartnerStack.Screen name="Cadastrar Promoção" component={RSale} />
-    <PartnerStack.Screen name=" Voucher" component={ValidQRCode} />
+    <PartnerStack.Screen name="Voucher" component={ValidQRCode} />
+    <PartnerStack.Screen name="MC" component={report} />
   </PartnerStack.Navigator>
 );
 export default PartnerRoutes;
